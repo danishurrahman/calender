85338dfa7d45403d1b4ffb2bad4c77f972e098db
Danish <danishurrahman3@gmail.com>
2022-11-02 19:50:30 +0530

    changes on spec

--- cal/src/app/app.component.spec.ts
+++ cal/src/app/app.component.spec.ts
@@ -25,8 +25,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('cal');
   });
-
-  it('should render title', () => {
+ it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
